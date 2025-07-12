68d1afef6c2fe07fd02b417bffe4541462ac9b3d
Nicolas <nicolasneal1@gmail.com>
2025-07-12 13:25:33 -0500

    First two items added.

--- src/main/java/net/nicolas/calcium/item/ModItems.java
+++ src/main/java/net/nicolas/calcium/item/ModItems.java
@@ -1,6 +1,8 @@
 package net.nicolas.calcium.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -12,7 +14,8 @@ import java.util.function.Function;
 public class ModItems {
     public static final String MOD_ID = "calcium";
 
-    public static final Item HIDE = register("hide", Item::new, new Item.Settings().maxCount(99));
+    public static final Item HIDE = register("hide", Item::new, new Item.Settings().maxCount(64));
+    public static final Item FUR = register("fur", Item::new, new Item.Settings().maxCount(64));
 
     private static <T extends Item> T register(String name, Function<Item.Settings, T> constructor, Item.Settings settings) {
         RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, name));
@@ -21,6 +24,9 @@ public class ModItems {
     }
 
     public static void initialize() {
-
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register((itemgroup) -> {
+            itemgroup.add(HIDE);
+            itemgroup.add(FUR);
+        });
     }
 }
